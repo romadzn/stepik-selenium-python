15abf640b4114d06f508364b95bca84cd6802345
Roman Romanov <romanoff280@yandex.ru>
2022-06-07 16:49:43 +0500

    add b2-l3-s5

---
+++ b2_lesson3_step6.py
@@ -0,0 +1,43 @@
+"""
+В этом задании после нажатия кнопки страница откроется в новой вкладке, нужно переключить WebDriver на новую вкладку и решить в ней задачу.
+
+Сценарий для реализации выглядит так:
+
+Открыть страницу http://suninjuly.github.io/redirect_accept.html
+Нажать на кнопку
+Переключиться на новую вкладку
+Пройти капчу для робота и получить число-ответ
+Если все сделано правильно и достаточно быстро (в этой задаче тоже есть ограничение по времени), 
+вы увидите окно с числом. Отправьте полученное число в качестве ответа на это задание.
+
+"""
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/redirect_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    browser.find_element_by_css_selector("button").click()
+    new_window = browser.window_handles[1]
+    browser.switch_to.window(new_window)
+    
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+
+    browser.find_element_by_class_name("btn.btn-primary").click()
+    
+finally:
+    time.sleep(5)
+    browser.quit()    
