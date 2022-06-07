e4dbc34a8a730c1529fb0a3a9c7f6292221f180a
Roman Romanov <romanoff280@yandex.ru>
2022-06-07 16:24:18 +0500

    add b2-l2-stp4

---
+++ b2_lesson3_step4.py
@@ -0,0 +1,43 @@
+"""
+В этой задаче вам нужно написать программу, которая будет выполнять следующий сценарий:
+
+Открыть страницу http://suninjuly.github.io/alert_accept.html
+Нажать на кнопку
+Принять confirm
+На новой странице решить капчу для роботов, чтобы получить число с ответом
+Если все сделано правильно и достаточно быстро (в этой задаче тоже есть ограничение по времени),
+вы увидите окно с числом. Отправьте полученное число в качестве ответа на это задание.
+
+"""
+
+from os import link
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    link = "http://suninjuly.github.io/alert_accept.html"
+    browser = webdriver.Chrome()
+    browser.get(link)
+    
+    browser.find_element_by_css_selector("button.btn").click()
+    alert = browser.switch_to.alert
+    alert.accept()
+    
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    
+    input1 = browser.find_element_by_id("answer")
+    input1.send_keys(y)
+    
+    browser.find_element_by_css_selector("button.btn").click()
+    
+finally:    
+    time.sleep(5)
+    browser.quit()
+    
