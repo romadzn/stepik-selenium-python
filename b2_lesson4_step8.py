194fae5ca0d35dd3ef38aaa4e88817e9e766b01f
Roman Romanov <romanoff280@yandex.ru>
2022-06-13 18:17:16 +0500

    add b2_l4-s8

---
+++ b2_lesson4_step8.py
@@ -0,0 +1,33 @@
+"""
+"""
+
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium import webdriver
+import time
+import math
+
+def calc(x):
+    return str(math.log(abs(12*math.sin(int(x)))))
+
+try:
+    browser = webdriver.Chrome()
+    browser.get("http://suninjuly.github.io/explicit_wait2.html")
+    browser.implicitly_wait(5)
+
+    WebDriverWait(browser, 12).until(EC.text_to_be_present_in_element((By.ID, "price"), "$100"))
+    browser.find_element(By.ID, "book").click()
+
+    browser.execute_script("window.scrollBy(0, 150);")
+        
+    x_element = browser.find_element_by_id("input_value")
+    x = x_element.text
+    y = calc(x)
+    input1 = browser.find_element_by_css_selector("input:required")
+    input1.send_keys(y) 
+    browser.find_element_by_id("solve").click()
+    
+finally:
+    time.sleep(100)
+    browser.quit()    
