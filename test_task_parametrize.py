eabefa8d1957afc6051cfb5c7135ff39736f227a
Roman Romanov <romanoff280@yandex.ru>
2022-06-22 14:49:40 +0500

    block 3 lesson 6 step 3 task

---
+++ test_task_parametrize.py
@@ -0,0 +1,32 @@
+import pytest
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+import math
+import time
+
+@pytest.fixture(scope="function")
+def browser():
+    print("\nstart browser for test..")
+    browser = webdriver.Chrome()
+    yield browser
+    print("\nquit browser..")
+    browser.quit()
+    
+@pytest.mark.parametrize("linknums", ["236895", "236896", "236897", "236898", "236899", "236903", "236904", "236905"])
+class TestPagesTask:
+    def test_links_parametrize(self, browser, linknums):
+        link = f"https://stepik.org/lesson/{linknums}/step/1"
+        browser.get(link)
+        browser.implicitly_wait(5)
+        
+        input1 = browser.find_element(By.TAG_NAME, "textarea")
+        input1.send_keys(str(math.log(int(time.time()))))
+        browser.implicitly_wait(5)
+        browser.find_element(By.CLASS_NAME, "submit-submission").click()
+        
+        checkval = browser.find_element(By.CLASS_NAME, "smart-hints__hint")
+        assert "Correct!" == checkval.text
+
+if __name__ == '__main__':
+    pytest.main()        
